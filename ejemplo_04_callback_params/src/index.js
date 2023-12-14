526ed967a087b60db7c23a74d9f14f79a4890834
Graimi <113901525+Graimi@users.noreply.github.com>
2023-12-14 17:03:31 +0100

    ejemplo_05_endpoint_get done

--- ejemplo_04_callback_params/src/index.js
+++ ejemplo_04_callback_params/src/index.js
@@ -10,40 +10,7 @@ app.use('/api', router);
 
 // http://localhost:${PORT}/api/ping GET
 router.get('/ping', (req, res, next) => {
-  //   Para ver todo lo que tiene una request
-  //   console.log(req);
-
-  //   Devuelve lo que esté detrás de la ?
-  //   http://localhost:${PORT}/api/ping?name=cristian // GET
-  req.query; // {name: 'cristian'}
-
-  //   Devuelve parte de la api, hay que definirlo en el router
-  //   En el ejemplo dará 1
-  //   http://localhost:${PORT}/api/students/1 // GET
-  //   router.get('/students/:id', (req, res, next) => {
-  //     res.params.id; // {id: 3}
-  //   });
-
-  //   POST PUT PATCH
-  req.body; // {...}
-
-  //   Para ver todo lo que tiene una response
-  //   console.log(res);
-
-  // Para establecer el status
-  res.status(200);
-
-  // Manda strings como documento interpretabla a HTML
-  //   res.send('');
-
-  // Esta es mejor opción, manda un json
-  //   No hay un estandar, se ponen algunos ejemplos
-  //   res.json({ data: {}, errors: {}});
-  res.json({ data: 'Pong!' });
-
-  //   Para saltar de un contenido a otro
-  next();
-  return;
+  res.status(200).send('Pong!');
 });
 
 const PORT = 3001;
---
+++ ejemplo_05_endpoint_get/src/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const app = express();
+
+// Creamos el soporte para peticiones tipo GET, POST, PUT, PATCH, DELETE
+const router = express.Router();
+
+// http://localhost:${PORT}/api/upper?name=jaime GET
+router.get('/upper', (req, res) => {
+  try {
+    const { name } = req.query;
+    const upperName = name.toUpperCase();
+    res.status(200).json({ data: `Te llamas ${upperName}` });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+// Fake database
+const students = [
+  {
+    name: 'Jaime',
+  },
+  {
+    name: 'Alberto',
+  },
+  {
+    name: 'Cristian',
+  },
+];
+
+// http://localhost:${PORT}/api/students GET
+router.get('/students', (req, res, next) => {
+  try {
+    const { filter } = req.query;
+
+    if (filter) {
+      const filteredStudents = students.filter((student) => {
+        return student.name.toUpperCase() === filter.toUpperCase();
+      });
+      res.status(200).json({ data: filteredStudents });
+    } else {
+      res.status(200).json({ data: students });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+// Middleware de rutas en /api: http://localhost:${PORT}/api
+app.use('/api', router);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Aplicación corriendo en el puerto http://localhost:${PORT}`);
+});
