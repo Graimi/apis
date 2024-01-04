9a98a5eaff750b4553fb9bb7fed9bd21710905f8
Graimi <113901525+Graimi@users.noreply.github.com>
2024-01-04 16:51:55 +0100

    error handle done

--- ejemplo_10_refactor_architecture/src/index.js
+++ ejemplo_10_refactor_architecture/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mainRouter = require('./routes')
+const mainRouter = require('./routes');
 
 const app = express();
 // Para que pueda usar un req Body
@@ -7,6 +7,17 @@ app.use(express.json());
 
 // Middleware de rutas en /api: http://localhost:${PORT}/api
 app.use('/api', mainRouter);
+
+// Controlador de rutas no encontradas
+app.use('*', (req, res, next) => {
+  res.status(404).json({ data: 'Not found' });
+});
+
+// Controlador de errores generales del servidor
+// error solo se usa aquí
+app.use((error, req, res, next) => {
+  res.status(500).json({ data: 'Internal Server Error' });
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
