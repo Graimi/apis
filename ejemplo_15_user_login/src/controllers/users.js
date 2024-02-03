0841c71d2f33fd5d6dbb9858799cb8a51e16cb4d
Graimi <113901525+Graimi@users.noreply.github.com>
2024-02-03 11:22:42 +0100

    ejemplo_15_user_login

---
+++ ejemplo_15_user_login/src/controllers/users.js
@@ -0,0 +1,48 @@
+const { signToken } = require('../config/jwt');
+const mongo = require('../repositories/mongo/users');
+const { hashPassword, verifyPassword } = require('../config/password');
+
+const registerUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hash = await hashPassword(password);
+
+    // Creamos el usuario y declaramos en la variable password el hash de la contraseña
+    const newUser = await mongo.createUserInDB({ email, password: hash });
+    res.status(201).json({ data: newUser });
+  } catch (err) {
+    console.log('>>> Error creating user: ', err);
+    res.status(400).json({ data: 'Error registering user' });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await mongo.getUserByEmailFromDB(email);
+  // Comprobamos si existe el usuario
+  if (!user) {
+    res.status(401).json({ data: 'User does not exist' });
+    return;
+  }
+
+  const isValidPassword = await verifyPassword(password, user.password);
+  // Comprobamos si la contraseña es válida
+  if (!isValidPassword) {
+    res.status(401).json({ data: 'Incorrect email or password' });
+    return;
+  }
+
+  // Se invoca a la librería con la funcion firma
+  const token = signToken({ id: user._id });
+  const { password: unUsedPassword, ...restUser } = user;
+  res.status(200).json({ data: { token, user: restUser } });
+};
+
+const getUser = async (req, res) => {
+  const { id } = req.user;
+  const user = await mongo.getUserByIdFromDB(id);
+  res.status(200).json({ data: user });
+};
+
+module.exports = { loginUser, registerUser, getUser };
---
+++ ejemplo_15_user_login/src/middlewares/authenticated.js
@@ -0,0 +1,31 @@
+var jwt = require('jsonwebtoken');
+const { verifyToken } = require('../config/jwt');
+
+// Ejemplo anterior
+const AUTH_TOKEN = 'MY_SECRET_TOKEN';
+
+// Ejemplo anterior
+const isAuthenticated = (req, res, next) => {
+  const { token } = req.query;
+  if (token === AUTH_TOKEN) {
+    next();
+    return;
+  } else {
+    res.status(401).json({ data: 'Wrong token authentication' });
+  }
+};
+
+const hasValidAuthJwt = (req, res, next) => {
+  try {
+    const { token } = req.query;
+    const payload = verifyToken(token);
+
+    req.user = payload;
+
+    next();
+  } catch (err) {
+    res.status(401).json({ data: 'No authenticated' });
+  }
+};
+
+module.exports = { isAuthenticated, hasValidAuthJwt };
